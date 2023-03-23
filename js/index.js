78c5b3fbaf75429415bf4665dd96cae416e77f6a
Artem Pichugin <lurnpen@icloud.com>
2023-03-23 20:31:27 +0300

    add dark-mode

--- js/index.js
+++ js/index.js
@@ -1,9 +1,15 @@
 const upButton = document.querySelector(".footer-up");
+const themeSwitch = document.querySelector('.header-switch');
 
-upButton.addEventListener('click', function(){
+upButton.addEventListener('click', function () {
   window.scrollTo({
     top: 0,
     left: 0,
     behavior: "smooth",
   });
+});
+
+themeSwitch.addEventListener('click', function (event) {
+  document.body.classList.toggle('dark-theme');
+  event.currentTarget.classList.toggle('checked');
 });
